82ee98eb2fc64ef3a048ee822b65f5980ca91657
JoeV22 <79062669+JoeV22@users.noreply.github.com>
2021-12-07 00:29:13 -0500

    extra
    extra

--- frontend/App.js
+++ frontend/App.js
@@ -1,166 +1,78 @@
 import React from "react";
-import { 
-  Input,
-  Stack,
-  Center,
-  TextArea,
-  Heading,
-  NativeBaseProvider, 
-  Box,
-  AspectRatio,
-  Image,
-  Text,
-  VStack,
-  FormControl,
-  Link,
-  Button,
-  HStack,
-  Pressable,
-  Icon,
-  IconButton,
-  StatusBar,
-  ScrollView,
-  Avatar,
-} from "native-base";
-import { MaterialIcons, Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
-import { SafeAreaView, StyleSheet } from 'react-native';
-//Navbar
-//import AppBar from './Navbar';
-
-export const Events = () => {
+//import {McIcon, McText} from '../components';
+import { View, Image, Button, StyleSheet, TextInput, Pressable, Text} from 'react-native';
+export const ResetPassword = () => {
   return (
-    <ScrollView>
-    <Stack space={2} w="100%">
-      <Center>
-        <Heading ml="-5">Club Name</Heading>
-      </Center>
-      <HStack space={2}>
-      <Avatar
-        bg="green.500"
-        source={{
-          uri: "https://pbs.twimg.com/profile_images/1188747996843761665/8CiUdKZW_400x400.jpg",
-        }}
-      >
-        SS
-      </Avatar>
-      <Avatar
-        bg="cyan.500"
-        source={{
-          uri: "https://pbs.twimg.com/profile_images/1177303899243343872/B0sUJIH0_400x400.jpg",
-        }}
-      >
-        GG
-      </Avatar>
-      <Avatar
-        bg="indigo.500"
-        source={{
-          uri: "https://pbs.twimg.com/profile_images/1352844693151731713/HKO7cnlW_400x400.jpg",
-        }}
-      >
-        RS
-      </Avatar>
-      <Avatar
-        bg="amber.500"
-        source={{
-          uri: "https://pbs.twimg.com/profile_images/1309797238651060226/18cm6VhQ_400x400.jpg",
-        }}
-      >
-        AK
-      </Avatar>
-      <Avatar
-        bg="amber.500"
-        source={{
-          uri: "https://pbs.twimg.com/profile_images/1188747996843761665/8CiUdKZW_400x400.jpg",
-        }}
-      >
-        AK
-      </Avatar>
-      <Avatar
-        bg="amber.500"
-        source={{
-          uri: "https://pbs.twimg.com/profile_images/1352844693151731713/HKO7cnlW_400x400.jpg",
-        }}
-      >
-        AK
-      </Avatar>
+    <View style={styles.app , {flex: 1, alignItems: 'center', justifyContent: 'center'}}>
 
-    </HStack>
-
-      <Center>
-        <Heading>Name of Event</Heading>
-      </Center>
-      <Box>
-      <AspectRatio ratio={16 / 9}>
           <Image
-            roundedTop="lg"
+            style={styles.Logo}
             source={{
-              uri: "https://www.messiah.edu/images/download_university.png",
+              uri: "https://www.nicepng.com/png/detail/207-2073361_reset-password-png-download-reset-password-icon-png.png",
             }}
-            alt="image"
           />
-        </AspectRatio>
-      </Box>
-<Box p="4" space={2}>
-    <Heading size="md" ml="-1">
-     Event Description
-    </Heading>
-   <Text
-     fontSize="xs"
-     _light={{ color: 'violet.500' }}
-     _dark={{ color: 'violet.300' }}
-     fontWeight="500"
-     ml="-0.5"
-     mt="-1"
-   >
-     Event Name
-   </Text>
-      <Text fontWeight="400">
-            So this is where we plan on writing the individual descriptions of the
-            events. Honestly not sure what else to write so this is filler.
-            Watch Marvel's what If highly recommend it. Also if you have time go 
-            check out the netflix marvel shows they're bomb!
-      </Text> 
-</Box>
-    </Stack>
-    </ScrollView>
+
+      <TextInput
+        style={styles.input}
+        value="username"
+      />
+      <TextInput
+        style={styles.input}
+        value="New Password"
+      />
+      <TextInput
+        style={styles.input}
+        value="Re-Enter Password"
+      />
+        <View paddingVertical={20} paddingHorizontal={75}>
+        <Pressable style={styles.button}>
+            <Text style={styles.text}>Reset Password</Text>
+          </Pressable>
+        </View>
+    </View>
   )
 }
+//StyleSheet
+const styles = StyleSheet.create({
+  app: {
+    backgroundColor: 'white'
 
+  },
+  back: {
+    backgroundColor: 'white'
 
-//Navbar Start
-function AppBar(){
-  return (
-    <>
-        <StatusBar backgroundColor="#3700B3" barStyle="light-content" />
+  },
+  heading: {
+    color: 'black'
+  },
+  input: {
+    width: 300,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#4C9A2A',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  Logo: {
+    width: 350,
+    height: 200,
+  },
+});
+export default ResetPassword;
 
-        <Box safeAreaTop backgroundColor="#6200ee" />
-
-        <HStack bg='#1A3D61' px={1} py={3} justifyContent='space-between' alignItems='center'>
-          <HStack space={4} alignItems='center'>
-            <IconButton icon={<Icon size="sm" as={<MaterialIcons name='menu' />} color="white" />} />
-            <Text color="white" fontSize={20} fontWeight='bold'>Home</Text>
-          </HStack>
-          <HStack space={2}>
-            <IconButton icon={<Icon as={<MaterialIcons name='favorite' />} size='sm' color="white" />} />
-            <IconButton icon={<Icon as={<MaterialIcons name='search' />}
-            color="white" size='sm'  />} />
-            <IconButton icon={<Icon as={<MaterialIcons name='more-vert' />} size='sm' color="white" />} />
-          </HStack>
-        </HStack>
-
-    </>
-  )
-}
-//navbar end
-
-export default () => {
-  return (
-    <NativeBaseProvider>
-      <Center flex={1} px="3">
-      <Events />
-      </Center>
-      <AppBar/>
-    </NativeBaseProvider>
-  )
-}
 
--- frontend/Events/EventsInfo.js
+++ frontend/Events/EventsInfo.js
@@ -1,117 +1,298 @@
 import React from "react";
+//Navbar
+//import AppBar from './Navbar';
+//
 import { 
   Input,
   Stack,
   Center,
-  TextArea,
   Heading,
   NativeBaseProvider, 
   Box,
   AspectRatio,
   Image,
-  Text,
-  VStack,
-  FormControl,
-  Link,
-  Button,
-  HStack,
-  Pressable,
+  Fab,
   Icon,
-  IconButton,
-  StatusBar,
-  ScrollView,
+  Text,
+  FlatList,
+  HStack,
   Avatar,
+  VStack,
+  Spacer,
+  StatusBar,
+  IconButton,
   View,
 } from "native-base";
-import { MaterialIcons, Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
-import { SafeAreaView, StyleSheet } from 'react-native';
-//Navbar
-//import AppBar from './Navbar';
-
-export const EventsInfo = () => {
+import { AntDesign, MaterialIcons } from "@expo/vector-icons"
+import { Button, ScrollView, StyleSheet } from 'react-native';
+import { alignContent, position } from "styled-system";
+import ImagedCarouselCard from "react-native-imaged-carousel-card";
+export const CabinetMembers = () => {
+  const data = [
+    {
+      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+      fullName: "Aafreen Khan",
+      timeStamp: "12:47 PM",
+      recentText: "Good Day!",
+      avatarUrl:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    },
+    {
+      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+      fullName: "Sujitha Mathur",
+      timeStamp: "11:11 PM",
+      recentText: "Cheer up, there!",
+      avatarUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEaZqT3fHeNrPGcnjLLX1v_W4mvBlgpwxnA&usqp=CAU",
+    },
+    {
+      id: "58694a0f-3da1-471f-bd96-145571e29d72",
+      fullName: "Anci Barroco",
+      timeStamp: "6:22 PM",
+      recentText: "Good Day!",
+      avatarUrl: "https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg",
+    },
+    {
+      id: "68694a0f-3da1-431f-bd56-142371e29d72",
+      fullName: "Aniket Kumar",
+      timeStamp: "8:56 PM",
+      recentText: "All the best",
+      avatarUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU",
+    },
+    {
+      id: "28694a0f-3da1-471f-bd96-142456e29d72",
+      fullName: "Kiara",
+      timeStamp: "12:47 PM",
+      recentText: "I will call today.",
+      avatarUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU",
+    },
+  ]
   return (
     <View style={styles.app} w="100%" h="100%" alignContent='center' alignItems='center' paddingTop='20'>
-    <Stack space={2} w="80%">
-      <Center>
-        <Heading style={styles.heading}>Event Name</Heading>
-      </Center>
-      <Box>
-      <AspectRatio ratio={16 / 9}>
-          <Image
-            roundedTop="lg"
-            source={{
-              uri: "https://www.messiah.edu/images/download_university.png",
+    <Stack space={2} w="100%">
+    <ScrollView>
+      <Heading fontSize="3xl" p="0" pb="0" style={styles.heading}>
+        Clubs
+      </Heading>
+
+    <HStack space={5} alignItems="center">
+    <ImagedCarouselCard
+      width={200}
+      height={200}
+      shadowColor="#051934"
+      source={{
+        uri: "https://wallpaperaccess.com/full/317501.jpg",
+             }}
+    />
+    <ImagedCarouselCard
+      width={200}
+      height={200}
+      shadowColor="#051934"
+      source={{
+        uri: "https://wallpaperaccess.com/full/317501.jpg",
+             }}
+    />
+    <ImagedCarouselCard
+      width={200}
+      height={200}
+      shadowColor="#051934"
+      source={{
+        uri: "https://wallpaperaccess.com/full/317501.jpg",
+             }}
+    />
+    <ImagedCarouselCard
+      width={200}
+      height={200}
+      shadowColor="#051934"
+      source={{
+        uri: "https://wallpaperaccess.com/full/317501.jpg",
+             }}
+    />
+    <ImagedCarouselCard
+      width={200}
+      text={"what"}
+      height={200}
+      shadowColor="#051934"
+      source={{
+        uri: "https://wallpaperaccess.com/full/317501.jpg",
+             }}
+    />
+
+    </HStack>
+      <Heading fontSize="3xl" p="0" pb="0" style={styles.heading}>
+        Discover
+      </Heading>
+      <HStack space={5} alignItems="center">
+       <Image
+          h='40' w='20' rounded='md' shadow={3}
+          source={{
+            uri: "https://wallpaperaccess.com/full/317501.jpg",
+              }}
+          alt="Alternate Text"
+      />
+      <Image
+          h='40' w='20' rounded='md' shadow={3}
+          source={{
+            uri: "https://wallpaperaccess.com/full/317501.jpg",
+              }}
+          alt="Alternate Text"
+      />
+      <Image
+          h='40' w='20' rounded='md' shadow={3}
+          source={{
+            uri: "https://wallpaperaccess.com/full/317501.jpg",
+              }}
+          alt="Alternate Text"
+      />
+      <Image
+          h='40' w='20' rounded='md' shadow={3}
+          source={{
+            uri: "https://wallpaperaccess.com/full/317501.jpg",
+              }}
+          alt="Alternate Text"
+      />
+      <Image
+          h='40' w='20' rounded='md' shadow={3}
+          source={{
+            uri: "https://wallpaperaccess.com/full/317501.jpg",
+              }}
+          alt="Alternate Text"
+      />
+      
+      </HStack>
+      <Box
+      w={{
+        base: "100%",
+        md: "25%",
+      }}
+     />
+      <Heading fontSize="xl" p="0" pb="0" style={styles.heading}>
+      Events
+      </Heading>
+      <Box padding='0' alignContent="space-around" alignItems="center" space={2}>
+      <FlatList
+      style={styles.container}
+      style={styles.back}
+        data={data}
+        renderItem={({ item }) => (
+          <Box
+            borderBottomWidth="1"
+            _dark={{
+              borderColor: "gray.600",
             }}
-            alt="image"
-          />
-        </AspectRatio>
+            borderColor="coolGray.200"
+            pl="4"
+            pr="2"
+            py="2"
+          >
+            <HStack space={3} justifyContent="space-between">
+              <Avatar
+                size="48px"
+                source={{
+                  uri: item.avatarUrl,
+                }}
+              />
+              <VStack>
+                <Text
+                  _dark={{
+                    color: "yellow.200",
+                  }}
+                  color="yellow.600"
+                  bold
+                >
+                  {item.fullName}
+                </Text>
+                <Text
+                  color="yellow.600"
+                  _dark={{
+                    color: "yellow.200",
+                  }}
+                >
+                  {item.recentText}
+                </Text>
+               <Box  space={10}>
+               <Image
+              source={{
+                uri: "https://wallpaperaccess.com/full/317501.jpg",
+                }}
+                alt="Alternate Text"
+                size="2xl"
+              />
+                <Heading size="md" ml="-1" style={styles.heading}>
+                  Event Description
+               </Heading>
+                 <Text fontWeight="400" style={styles.heading} alignContent="flex-start" paddingRight="200">
+                   So this is where we plan on writing the individual 
+                   descriptions of the events. Honestly not sure what 
+                   else to write so this is filler. Watch Marvel's what
+                   If highly recommend it. Also if you have time go 
+                   check out the netflix marvel shows they're bomb!
+                </Text> 
+                </Box>
+              </VStack>
+              <Spacer />
+              <Text
+                fontSize="xs"
+                _dark={{
+                  color: "warmGray.50",
+                }}
+                color="coolGray.800"
+                alignSelf="flex-start"
+              >
+                {item.timeStamp}
+              </Text>
+            </HStack>
+          </Box>
+        )}
+        keyExtractor={(item) => item.id}
+      />
       </Box>
-<Box p="4" space={20}>
-    <Heading size="md" ml="-1" style={styles.heading}>
-     Event Description
-    </Heading>
-      <Text fontWeight="400" style={styles.heading}>
-            So this is where we plan on writing the individual descriptions of the
-            events. Honestly not sure what else to write so this is filler.
-            Watch Marvel's what If highly recommend it. Also if you have time go 
-            check out the netflix marvel shows they're bomb!
-      </Text> 
-</Box>
+      <HStack space={3} alignItems="center">
+
+      </HStack>
+      <Box
+      w={{
+        base: "100%",
+        md: "25%",
+      }}
+     />
+      </ScrollView>
     </Stack>
     </View>
+    
   )
 }
 
-
-//Navbar Start
-function AppBar(){
-  return (
-    <>
-        <StatusBar backgroundColor="#3700B3" barStyle="light-content" />
-
-        <Box safeAreaTop backgroundColor="#6200ee" />
-
-        <HStack bg='#1A3D61' px={1} py={3} justifyContent='space-between' alignItems='center'>
-          <HStack space={4} alignItems='center'>
-            <IconButton icon={<Icon size="sm" as={<MaterialIcons name='menu' />} color="white" />} />
-            <Text color="white" fontSize={20} fontWeight='bold'>Home</Text>
-          </HStack>
-          <HStack space={2}>
-            <IconButton icon={<Icon as={<MaterialIcons name='favorite' />} size='sm' color="white" />} />
-            <IconButton icon={<Icon as={<MaterialIcons name='search' />}
-            color="white" size='sm'  />} />
-            <IconButton icon={<Icon as={<MaterialIcons name='more-vert' />} size='sm' color="white" />} />
-          </HStack>
-        </HStack>
-
-    </>
-  )
-}
-//navbar end
-//StyleSheet
+//Style's Section
 const styles = StyleSheet.create({
   app: {
-    backgroundColor: '#F9FC78'
-    ,position: 'relative'
+    backgroundColor: 'black'
 
   },
   back: {
-    backgroundColor: 'white'
+    backgroundColor: 'black'
 
   },
   heading: {
     color: 'white'
   },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
 
 });
-//Export
+//Export Section
 export default () => {
   return (
     <NativeBaseProvider>
       <Center flex={1}>
-      <EventsInfo />
+        <CabinetMembers />
       </Center>
-      <AppBar/>
     </NativeBaseProvider>
   )
 }
