dee1e3363a8895048053772ecb3b1d16b69bf500
JoeV22 <79062669+JoeV22@users.noreply.github.com>
2021-12-07 00:28:06 -0500

    Details
    -some new node modules installed too.
    -forgot password
    -reset password
    -event details

--- screens/EventDetails.js
+++ screens/EventDetails.js
@@ -1,12 +1,13 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, ScrollView, Text, StyleSheet} from 'react-native';
+import {View, Pressable, ScrollView, Text, StyleSheet, Image} from 'react-native';
 import {COLORS, FONTS, icons, images, SIZES} from '../constants';
 import {McIcon, McText} from '../components';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 const EventDetails = ({route, navigation}) => {
   const {item} = route.params;
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
@@ -14,9 +15,25 @@ const EventDetails = ({route, navigation}) => {
           <McText h1 style={{marginBottom: 20}}>
             Event Details
           </McText>
-          <McText h2>{item.eventName}</McText>
-          <McText body3>{item.clubName}</McText>
-          <McText body4>{item.desc}</McText>
+          <View style={styles.eventdetails} paddingVertical={50} paddingHorizontal={100}>
+
+          <McText h2 style={{marginBottom:10}}>{item.eventName}</McText>
+          <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://loveincorporated.blob.core.windows.net/contentimages/gallery/70bc81c8-b277-407d-8c3a-5c1a3e501732-4-hamburger.jpg',
+        }}
+      />
+          <McText body2 style={{marginTop:20}}>Club: {item.clubName}</McText>
+          <McText body3> Synopsis: </McText>
+          <McText body4> {item.desc}</McText>
+        </View>
+        <View style={styles.eventdetails} paddingVertical={50} paddingHorizontal={138}>
+        <Pressable style={styles.button}>
+            <Text style={styles.text}>Signup</Text>
+          </Pressable>
+        </View>
+
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -27,6 +44,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.black,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#4C9A2A',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  heading:{
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  eventdetails:{
+    backgroundColor: '#18191A',
+  },
+  tinyLogo: {
+    width: 350,
+    height: 200,
   },
 });
 
---
+++ screens/ForgotPassword.js
@@ -0,0 +1,82 @@
+import React from "react";
+import {McIcon, McText} from '../components';
+import {COLORS, FONTS, icons, images, SIZES} from '../constants';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import { View, Image, Button, StyleSheet, TextInput, Pressable, Text, ScrollView} from 'react-native';
+export const ForgotPassword = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+    <ScrollView>
+    <View style={styles.app , {flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <McText h1 style={{marginBottom: 20}}>
+            Forgot Password
+          </McText>
+
+          <Image
+            style={styles.Logo}
+            source={{
+              uri: "https://www.nicepng.com/png/detail/207-2073361_reset-password-png-download-reset-password-icon-png.png",
+            }}
+          />
+
+      <TextInput
+        style={styles.input}
+        value="Email"
+      />
+        <View paddingVertical={20} paddingHorizontal={75}>
+        <Pressable style={styles.button}>
+            <Text style={styles.text}>Send Reset Link</Text>
+          </Pressable>
+        </View>
+    </View>
+    </ScrollView>
+    </SafeAreaView>
+  )
+}
+//StyleSheet
+const styles = StyleSheet.create({
+  app: {
+    backgroundColor: 'white'
+
+  },
+  container: {
+    flex: 1,
+  },
+  back: {
+    backgroundColor: 'white'
+
+  },
+  heading: {
+    color: 'black'
+  },
+  input: {
+    width: 300,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#4C9A2A',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  Logo: {
+    width: 350,
+    height: 200,
+  },
+});
+export default ForgotPassword;
+
+
