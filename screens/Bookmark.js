620899bfb8080e1f0c4599c903cfc9ae4992a8af
JoeV22 <79062669+JoeV22@users.noreply.github.com>
2022-02-15 02:03:12 -0500

    testing some things

--- screens/Bookmark.js
+++ screens/Bookmark.js
@@ -1,81 +1,20 @@
 import React from 'react';
-import {View, StyleSheet, ScrollView, Image} from 'react-native';
-import { Button, NativeBaseProvider, Center, Box, Heading, FlatList, HStack, VStack, Spacer, Avatar, Text, AspectRatio} from "native-base";
+import {View, StyleSheet, Image, Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {ScrollView} from 'react-native-gesture-handler';
+import {COLORS, FONTS, icons, images, SIZES} from '../constants';
 
-const Place = () => {
+const Asa = () => {
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Button>Click Me</Button>
-        <Image
-              style={styles.tinyLogo}
-              source={{
-                uri: 'https://loveincorporated.blob.core.windows.net/contentimages/gallery/70bc81c8-b277-407d-8c3a-5c1a3e501732-4-hamburger.jpg',
-              }}
-            />
-    </View>
+    <SafeAreaView>
+    <ScrollView>
+    <Text>
+        Club: ASA
+    </Text>
+    </ScrollView>
+    </SafeAreaView>
   );
 };
 
-//Style's Section
-const styles = StyleSheet.create({
-  app: {
-    backgroundColor: '#6200ee'
-    ,position: 'relative'
-
-  },
-  back: {
-    backgroundColor: 'white'
-
-  },
-  heading: {
-    color: 'black'
-  },
-  tinyLogo: {
-    width: 50,
-    height: 50,
-  },
-});
-
-export default () => {
-  return (
-    <NativeBaseProvider>
-      <Center flex={1}>
-        <Place />
-      </Center>
-    </NativeBaseProvider>
-  )
-}
-/*
-    <ScrollView horizontal={true} >
-    <Box>
-      <AspectRatio ratio={16 / 9}>
-    <Image
-            roundedTop="lg"
-            source={{
-              uri: "https://i.pinimg.com/originals/3f/94/70/3f9470b34a8e3f526dbdb022f9f19cf7.jpg",
-            }}
-            alt="ProfileImage"
-          />
-        </AspectRatio>
-      </Box>
-      </ScrollView>
-
-   <HStack space={3} justifyContent="center">
-    <Image
-    source={{ uri: "https://wallpaperaccess.com/full/317501.jpg", }}
-    width={45}
-    height={75}
-   />
-    <Image
-    source={{ uri: "https://wallpaperaccess.com/full/317501.jpg", }}
-    width={45}
-    height={75}
-    />
-      <Image
-    source={{ uri: "https://wallpaperaccess.com/full/317501.jpg", }}
-    width={45}
-    height={75}
-   />
-    </HStack>
-
-*/
+export default Asa;
