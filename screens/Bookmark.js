a301c5b37782be75b3f97fbf859d4de28d2e604d
JoeV22 <79062669+JoeV22@users.noreply.github.com>
2022-02-15 04:01:52 -0500

    Figuring out Styles
    Got it to run styles component

--- screens/Bookmark.js
+++ screens/Bookmark.js
@@ -3,15 +3,23 @@ import {View, StyleSheet, Image, Text} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {ScrollView} from 'react-native-gesture-handler';
 import {COLORS, FONTS, icons, images, SIZES} from '../constants';
+import {DiscoverContainer, Line, SubHeader} from '../constants/styles';
+import AppleHeader from 'react-native-apple-header';
+import {StatusBar} from 'expo-status-bar';
+import ClubCard from '../components/ClubCard';
+
 
 const Asa = () => {
 
   return (
     <SafeAreaView>
     <ScrollView>
-    <Text>
-        Club: ASA
-    </Text>
+    <AppleHeader largeTitle="Asa" />
+    <StatusBar />
+    <ScrollView alignItems="center" horizontal={false} showsHorizontalScrollIndicator={false}>
+          <ClubCard image={images.asa} />
+    </ScrollView>
+
     </ScrollView>
     </SafeAreaView>
   );
