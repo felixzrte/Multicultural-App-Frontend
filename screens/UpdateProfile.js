ea8e197bcd5cd03457cdbb216d2aa3e8f243f062
el1303 <56045258+el1303@users.noreply.github.com>
2022-02-17 20:13:18 -0500

    Update UpdateProfile.js

--- screens/UpdateProfile.js
+++ screens/UpdateProfile.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, {useState} from 'react';
 
 import {
   StyleSheet,
@@ -9,13 +9,64 @@ import {
   Title,
   Caption,
   Text,
-  TouchableRipple,
+  FlatList,
+  Modal,
+  TouchableOpacity,
+  TextInput,
 } from 'react-native';
 import {Ionicons, MaterialIcons} from '@expo/vector-icons';
 import { LinearGradient } from 'react-native-svg';
 import Profile from './Profile';
+import { initialWindowMetrics } from 'react-native-safe-area-context';
+
+
+const DATA = [
+  {id: 1, text: 'Name'},
+  {id: 2, text: 'Email'},
+  {id: 3, text: 'Password'},
+]
 
 const UpdateProfile = () => {
+  const [data, setdata] = useState(DATA)
+  const [isRender, setisRender] = useState(false);
+  const [isModalVisible, setisModalVisible] = useState(false);
+  const [inputText, setinputText] = useState();
+  const [editItem, seteditItem] = useState();
+
+  const onPressItem = (item) => {
+    setisModalVisible(true);
+    setinputText(item.text)
+    seteditItem(item.id)
+  }
+  
+
+  const renderItem = ({item, index}) => {
+  return (
+    <TouchableOpacity
+      style={styles.item}
+      onPress={() => onPressItem(item)}
+    >
+      <Text style={styles.text}>{item.text}</Text>
+    </TouchableOpacity>
+  )
+  }
+
+  const handleEditItem = (editItem) => {
+    const newData = data.map(item => {
+      if (item.id == editItem) {
+        item.text = inputText;
+        return item;
+      }
+      return item;
+    })
+    setdata(newData);
+    setisRender(!isRender);
+  }
+
+  const onPressSaveEdit = () => {
+    handleEditItem(editItem); //save input text to data
+    setisModalVisible(false); // close modal
+  }
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.userInfoSection}>
@@ -25,8 +76,43 @@ const UpdateProfile = () => {
               style={styles.image}
               resizeMode="center"
             />
-           
           </View>
+          <FlatList
+            data={data}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={renderItem}
+            extraData={isRender}
+          />
+          <Modal
+            animationType='fade'
+            visible={isModalVisible}
+            onRequestClose={() => setisModalVisible(false)}
+          > 
+            <View style={styles.modalView}>
+              <Text style={styles.text}>
+                Change Text: 
+              </Text>
+            <TextInput
+                style={styles.textInput}
+                onChangeText={(text) => setinputText(text)}
+                defaultValue={inputText}
+                editable={true}
+                multiline={false}
+                maxLength={100}
+
+              />
+            <TouchableOpacity
+                onPress={() => onPressSaveEdit()}
+                style={styles.touchableSave} >
+                <Text style={styles.text}>
+                  Save
+                </Text>
+              </TouchableOpacity>
+
+            </View>
+
+          </Modal>
+          
       </View>
     </SafeAreaView>
   );
@@ -37,9 +123,12 @@ const styles = StyleSheet.create({
 
   text: {
     fontFamily: 'HelveticaNeue',
-    color: 'white',
-    fontWeight: '400',
-    fontSize: 40,
+    color: 'black',
+    alignItems:'flex-start',
+    fontSize: 20,
+    borderBottomWidth: 1,
+    marginLeft: 10,
+    marginVertical: 50,
   },
   userInfoSection:{
     alignSelf: 'center',
@@ -57,17 +146,36 @@ const styles = StyleSheet.create({
     width: undefined,
     height: undefined,
   },
-  title: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 24,
-    marginHorizontal: 16,
+  container: {
+    flex: 1,
   },
-  caption: {
-    alignSelf: 'center',
+  item: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey',
+    alignItems: 'flex-start'
+  },
+  textInput:{
+    width: '70%',
+    height: 70,
+    borderColor: 'grey',
+    borderWidth: 1,
+    fontSize: 25
+  },
+  touchableSave:{
+    backgroundColor: 'blue',
+    paddingHorizontal: 25,
     alignItems: 'center',
-    marginTop: 16,
+    marginTop: 20
   },
-  
+  TouchableOpacity:{
+    backgroundColor: 'blue',
+    paddingHorizontal: 100,
+    alignItems: 'center',
+    marginTop: 20
+  },
+  modalView: {
+    flex:1,
+    alignItems: 'center',
+  }
 });
 export default UpdateProfile;
