8462199302c9ff6d338c33d8f76050a96d9330c5
JoeV22 <79062669+JoeV22@users.noreply.github.com>
2022-03-02 22:53:35 -0500

    Event Details

--- screens/EventDetails.js
+++ screens/EventDetails.js
@@ -17,6 +17,46 @@ const EventDetails = ({route, navigation}) => {
   const {item} = route.params;
 
   return (
+    <ScrollView>
+    <View style={{flex: 0, alignItems: 'center', justifyContent: 'center'}}>
+          <View style={styles.productItemContainer}>
+        <Image
+          source={{uri: item.pic}}
+          style={{
+            height: ITEM_HEIGHT,
+            width: ITEM_WIDTH,
+            borderRadius: SIZES.radius,
+          }}
+        />
+        </View>
+        </View>
+        <View style={styles.leftMargin}>
+        <McText h1>{item.merchItemName}</McText>
+        <McText></McText>
+        <McText h2>${item.merchItemPrice}</McText>
+        <McText></McText>
+        <McText h2>About This Product</McText>
+        <McText></McText>
+        <McText style={styles.descText}>{item.description}</McText>
+        <McText></McText>
+        <McText h2>Contact to Purchase</McText>
+        <McText style={styles.descText}>Email: {item.contactEmail}</McText>
+        <McText style={styles.descText}>Phone Number: {item.contactNumber}</McText>
+        <McText></McText>
+        <McText h2>Number of Items in Stock</McText>
+        <McText style={styles.descText}>Small: {item.numSmall}</McText>
+        <McText style={styles.descText}>Medium: {item.numMedium}</McText>
+        <McText style={styles.bottomText}>Large: {item.numLarge}</McText>
+        </View>
+     
+    
+  </ScrollView>
+  );
+};
+
+
+export default EventDetails;
+/* 
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
@@ -53,8 +93,5 @@ const EventDetails = ({route, navigation}) => {
         </View>
       </ScrollView>
     </SafeAreaView>
-  );
-};
 
-
-export default EventDetails;
+*/
