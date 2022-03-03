72b1199c1e47b90532db85aff6dca8c784a2e891
JoeV22 <79062669+JoeV22@users.noreply.github.com>
2022-03-02 22:47:28 -0500

    styles

--- components/EventDetailsStyles.js
+++ components/EventDetailsStyles.js
@@ -3,35 +3,65 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: COLORS.black,
-    },
-    button: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 12,
-      paddingHorizontal: 32,
-      borderRadius: 4,
-      elevation: 3,
-      backgroundColor: '#4C9A2A',
-    },
-    text: {
-      fontSize: 16,
-      lineHeight: 21,
-      fontWeight: 'bold',
-      letterSpacing: 0.25,
-      color: 'white',
-    },
-    heading: {
-      fontWeight: 'bold',
-      color: 'white',
-    },
-    eventdetails: {
-      backgroundColor: '#18191A',
-    },
-    tinyLogo: {
-      width: 200,
-      height: 200,
-    },
-  });
+        flex: 1,
+      },
+      headerContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      merchItemContainer: {
+        backgroundColor: '#d3d3d3',
+        marginBottom: 10,
+        borderRadius: 10,
+      },
+      productItemContainer: {
+        backgroundColor: '#d3d3d3',
+        marginBottom: 10,
+        borderRadius: 40,
+        borderTopRightRadius:0,
+        borderTopLeftRadius:0,
+      },
+      leftMargin: {
+        marginLeft: 25,
+      },
+      center: {
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 12,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: '#4C9A2A',
+      },
+      descText: {
+        fontSize: 14,
+        letterSpacing: 0,
+        color: 'black',
+      },
+      bottomText: {
+        fontSize: 14,
+        letterSpacing: 0,
+        color: 'black',
+        marginBottom: 30,
+      },
+      buttonText: {
+        fontSize: 14,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
+      },
+      heading: {
+        fontWeight: 'bold',
+        color: 'white',
+      },
+      tinyLogo: {
+        width: 350,
+        height: 200,
+      },
+    });
   
