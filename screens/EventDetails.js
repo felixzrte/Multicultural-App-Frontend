2766393a3d7c5a732a73a3d1a92438de685e45af
JoeV22 <79062669+JoeV22@users.noreply.github.com>
2022-03-22 07:52:59 -0400

    Small Changes - styling

--- screens/EventDetails.js
+++ screens/EventDetails.js
@@ -12,7 +12,7 @@ import {
 import {COLORS, FONTS, icons, images, SIZES} from '../constants';
 import {McIcon, McText} from '../components';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import styles from '../components/MerchProductStyles.js';
+import styles from '../components/EventDetailsStyles.js';
 
 const ITEM_WIDTH = SIZES.width * 1;
 const ITEM_HEIGHT = ITEM_WIDTH * 1;
@@ -36,17 +36,29 @@ const EventDetails = ({route, navigation}) => {
           </View>
         <View style={styles.leftMargin}>
         <McText h1>Club: {item.club}</McText>
-        <McText h1> Event Name: {item.eventName}</McText>
+        <McText></McText>
+        <McText h1>{item.eventName}</McText>
         <McText></McText>
         <McText h2>About This Event</McText>
         <McText></McText>
-        <McText style={styles.descText}> Description: {item.desc}</McText>
-        <McText></McText>
-        <McText style={styles.descText} > Date and Time: {item.date}</McText>
-        <McText style={styles.descText} > Location: {item.location}</McText>
-        <McText style={styles.descText} > Attendence: {item.attendence}</McText>
-        <McText style={styles.descText} > Favorite: {item.favorite}</McText>
+        <View style={styles.flatlistcontainer}>
+           <McText style={styles.descText}> Description: {item.desc}</McText>
+        </View>
+        <View style={styles.flatlistcontainer}> 
+        <McText style={styles.dText} > Date and Time: {item.date}</McText>
+        </View>
+        <View style={styles.flatlistcontainer}>
+        <McText style={styles.dText} > Location: {item.location}</McText>
+        </View>
+        <View style={styles.flatlistcontainer}>
+        <McText style={styles.dText} > Attendence: {item.attendence}</McText>
+        </View>
+        <View style={styles.flatlistcontainer}>
+        <McText style={styles.dText} > Favorite: {item.favorite}</McText>
+        </View>
+        <View style={styles.flatlistcontainer}>
         <McText style={styles.bottomText}> Extra Notes: {item.extraNotes}</McText>
+        </View>
         </View>
       </ScrollView>
   );
