9a06cf5798f941508509f3653aeadb316b22614e
Felix <felixzarate14@gmail.com>
2022-03-22 08:56:45 -0400

    More

--- screens/Product.js
+++ screens/Product.js
@@ -21,40 +21,42 @@ const Product = ({route, navigation}) => {
   const {item} = route.params;
 
   return (
-      <ScrollView>
-        <View style={{flex: 0, alignItems: 'center', justifyContent: 'center'}}>
-              <View style={styles.productItemContainer}>
-            <Image
-              source={{uri: item.pic}}
-              style={{
-                height: ITEM_HEIGHT,
-                width: ITEM_WIDTH,
-                borderRadius: SIZES.radius,
-              }}
-            />
-            </View>
-            </View>
-            <View style={styles.leftMargin}>
-            <McText h1>{item.merchItemName}</McText>
-            <McText></McText>
-            <McText body2>${item.merchItemPrice}</McText>
-            <McText></McText>
-            <McText h2>About This Product</McText>
-            <McText></McText>
-            <McText style={styles.descText}>{item.description}</McText>
-            <McText></McText>
-            <McText h2>Contact to Purchase</McText>
-            <McText style={styles.descText}>Email: {item.contactEmail}</McText>
-            <McText style={styles.descText}>Phone Number: {item.contactNumber}</McText>
-            <McText></McText>
-            <McText h2>Number of Items in Stock</McText>
-            <McText style={styles.descText}>Small: {item.numSmall}</McText>
-            <McText style={styles.descText}>Medium: {item.numMedium}</McText>
-            <McText style={styles.bottomText}>Large: {item.numLarge}</McText>
-            </View>
-         
-        
-      </ScrollView>
+    <ScrollView>
+      <View style={{flex: 0, alignItems: 'center', justifyContent: 'center'}}>
+        <View style={styles.productItemContainer}>
+          <Image
+            source={{uri: item.pic}}
+            style={{
+              height: ITEM_HEIGHT,
+              width: ITEM_WIDTH,
+              borderRadius: SIZES.radius,
+            }}
+          />
+        </View>
+      </View>
+      <View style={styles.leftMargin}>
+        <McText h1>{item.merchItemName}</McText>
+        <McText />
+        <McText body2 color={COLORS.gray1}>
+          ${item.merchItemPrice}
+        </McText>
+        <McText />
+        <McText h2>About This Product</McText>
+        <McText />
+        <McText style={styles.descText}>{item.description}</McText>
+        <McText />
+        <McText h2>Contact to Purchase</McText>
+        <McText style={styles.descText}>Email: {item.contactEmail}</McText>
+        <McText style={styles.descText}>
+          Phone Number: {item.contactNumber}
+        </McText>
+        <McText />
+        <McText h2>Number of Items in Stock</McText>
+        <McText style={styles.descText}>Small: {item.numSmall}</McText>
+        <McText style={styles.descText}>Medium: {item.numMedium}</McText>
+        <McText style={styles.bottomText}>Large: {item.numLarge}</McText>
+      </View>
+    </ScrollView>
   );
 };
 
